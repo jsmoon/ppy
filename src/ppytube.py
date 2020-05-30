a8c63b1ed88a7427497dea8de90d61d3e51bddef
Jeongseok Moon <js69.moon@gmail.com>
2020-05-30 15:26:50 +0900

    Added mime_type filter options

--- src/ppytube.py
+++ src/ppytube.py
@@ -14,7 +14,7 @@ from pytube import YouTube
 import logging
 logger = logging.getLogger(__name__)
 
-def ppytube_download(url):
+def ppytube_download(url: str, mime_type: str):
     try:
         from pytube import extract
         extract.video_id(url)
@@ -29,18 +29,20 @@ def ppytube_download(url):
     except Exception as e:
         sys.exit("YouTube Init Error -- {}\n{}".format(url, e))
 
-    if len(yt.streams.all()) == 0:
+    if len(yt.streams) == 0:
         sys.exit("No streams")
 
     try:
         print('{}'.format(yt.title))
-        stream = yt.streams.first()
-        if yt.streams.count() > 1:
+        streams = [st for st in yt.streams if st.mime_type.startswith(mime_type)]
+        stream = streams[0]
+        if len(streams) > 1:
             from pprint import pprint
-            pprint(yt.streams.all())
+            for st in streams:
+                pprint(st)
             itag = input("Select itag (default:{}): ".format(stream.itag))
             if itag:
-                stream = [st for st in yt.streams.all() if st.itag is int(itag)][0]
+                stream = [st for st in streams if st.itag is int(itag)][0]
     except Exception as e:
         sys.exit("No stream specified\n{}".format(e))
 
@@ -56,10 +58,21 @@ def ppytube_download(url):
 SHORTDESC="""download a video from youtube url using by pytube3."""
 
 if __name__ == "__main__":
-    logger.debug(sys.argv)
-    if len(sys.argv) <= 1:
-        sys.argv.append(input("Please input YouTube URL: "))
+    import argparse
+    parser = argparse.ArgumentParser()
+    # parse mime_type filter option
+    mime_group = parser.add_mutually_exclusive_group()
+    mime_group.add_argument("--audio", help="choose audio only", action="store_true")
+    mime_group.add_argument("--video", help="choose video only", action="store_true")
+    mime_group.add_argument("--mime", help="filter mime_type (video/mp4, audio/mp4, ...)")
 
-    for url in sys.argv[1:]:
+    logger.debug(sys.argv)
+    args, argv = parser.parse_known_args(sys.argv[1:])
+    mime = 'audio' if args.audio else 'video' if args.video else args.mime if args.mime else ''
+
+    if len(argv) < 1:
+        argv.append(input("Please input YouTube URL: "))
+
+    for url in argv:
         print("URL: {}".format(url))
-        ppytube_download(url)
+        ppytube_download(url, mime)
